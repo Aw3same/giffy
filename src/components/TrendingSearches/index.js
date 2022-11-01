1d01c1e86ed25c6f66013c853bfe042f2f2c20a8
Ángel Barroso <angel.barroso19@gmail.com>
2022-11-01 10:39:32 +0100

    Added hook for lazy loading

--- src/components/TrendingSearches/index.js
+++ src/components/TrendingSearches/index.js
@@ -1,13 +1,49 @@
-import { useEffect, useState } from "react";
-import getTrendingTerms from "services/getTrendingTermsService";
+import { useEffect, useState, useRef } from 'react'
+import getTrendingTerms from 'services/getTrendingTermsService'
 import Category from '../Category'
 
-export default function TrendingSearches() {
-    const [trends, setTrends] = useState([])
+function TrendingSearches() {
+  const [trends, setTrends] = useState([])
 
-    useEffect(() => {
-        getTrendingTerms().then(setTrends)
-    }, [])
+  useEffect(() => {
+    getTrendingTerms().then(setTrends)
+  }, [])
 
-  return <Category name='Tendencias' options={trends}/>
+  return <Category name='Tendencias' options={trends} />
+}
+
+function useNearScreen({ distance = '100px' } = {}) {
+  const [isNearScreen, setShow] = useState(false)
+  const fromRef = useRef()
+  useEffect(() => {
+    let observer
+
+    const onChange = (entries, observer) => {
+      const el = entries[0]
+      if (el.isIntersecting) {
+        setShow(true)
+        observer.disconnnect()
+      }
+    }
+
+    Promise.resolve(
+      typeof IntersectionObserver !== 'undefined'
+        ? IntersectionObserver
+        : import('intersection-observer')
+    ).then(() => {
+      observer = new IntersectionObserver(onChange, {
+        rootMargin: distance,
+      })
+
+      observer.observe(fromRef.current)
+    })
+    return () => observer && observer.disconnect()
+  })
+  return { isNearScreen, fromRef }
+}
+// min 51
+export default function LazyTrending() {
+  const { isNearScreen, fromRef } = useNearScreen({ distance: '200px' })
+
+  return <div ref={fromRef}>{isNearScreen ? <TrendingSearches /> : null}</div>
 }
