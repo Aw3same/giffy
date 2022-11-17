258c5fe8365a6fbdad6619c65e7bbf89cb34cd0c
Ángel Barroso Sanz <angel.barroso@linguaserve.com>
2022-11-17 17:39:24 +0100

    Create SearchWord component

---
+++ src/components/SearchForm/index.js
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+export default function SearchForm({ onSubmit }) {
+  const [keyword, setKeyword] = useState('')
+
+  const handleSubmit = evt => {
+    evt.preventDefault()
+    onSubmit({ keyword })
+  }
+
+  const handleChange = evt => {
+    setKeyword(evt.target.value)
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className='c-search'>
+      <button className='c-search-btn'>Buscar</button>
+      <input
+        className='c-search-input'
+        placeholder='Search a gif here...'
+        onChange={handleChange}
+        type='text'
+        value={keyword}
+      />
+    </form>
+  )
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,35 +1,20 @@
-import { useState } from 'react'
 import { useLocation } from 'wouter'
 import ListOfGifs from 'components/ListOfGifs/ListOfGifs'
 import { useGifs } from 'hooks/useGifs'
 import TrendingSearches from 'components/TrendingSearches'
+import SearchForm from 'components/SearchForm'
 
 export default function Home() {
-  const [keyword, setKeyword] = useState('')
   const [path, pushLocation] = useLocation()
   const { loading, gifs } = useGifs()
 
-  const handleSubmit = evt => {
-    evt.preventDefault()
-    // navegar a otra ruta
+  const handleSubmit = ({ keyword }) => {
     pushLocation(`/search/${keyword}`)
-  }
-
-  const handleChange = evt => {
-    setKeyword(evt.target.value)
   }
 
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <button>Buscar</button>
-        <input
-          placeholder='Search a gif here...'
-          onChange={handleChange}
-          type='text'
-          value={keyword}
-        />
-      </form>
+      <SearchForm onSubmit={handleSubmit} />
       <div className=''>
         <div className='App-main'>
           <div className='App-results'>
